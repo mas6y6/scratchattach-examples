9344f07c43593d16cef6b3886d895401098ae5d3
BuildTools <unconfigured@null.spigotmc.org>
2023-08-23 14:59:55 -0500

    Fixed

--- Profile Viewer Basic/example.py
+++ Profile Viewer Basic/example.py
@@ -1,11 +1,11 @@
 import scratchattach as scratch3 #Import the scratchattach module
 
 #Logins to your scratch account using sessionid
-session = scratch3.Session(".eJxVkEtPwzAQhP9LzjTEdvzqrZU4IYGIkApcIsdeNyaNXcWuykP8d2wpl15nZr_Z3d_qEmHxaoZqW80qsm9W3VUpTOCzYIRAjFFMKOYtQc1gueCCMo0ZGMTF9qv7GHfp7cW-glvU8-Ghe3Rs08oDjxlzCkfnN-6cSUTUqCE1JrJGCGevV5c09qW8dyYHOBW4lYRky3wqfwx9cjP8BF8W282wOK3un-Dav4dlup0fVRxzCEklDVKSSI6ZUQYjqxWxzaCJQLTF0oLhtLHlPohJhzC5Ar9mIJhb5KB0_kDZq2jgU25PLvh6NWLdwfm0ivs1_PcPuABqDw:1qUtFi:qT1j73gwy-ix2YANcvjHE80ptk4", username="Python")
+session = scratch3.Session("session_id", username="Python")
 
 #Replace "session_id" with your session id 
 #Use https://github.com/TimMcCool/scratchattach/wiki/Get-your-session-id to get session id of your account
-conn = session.connect_cloud("885272042") #replace the text with your project id
+conn = session.connect_cloud("project_id") #replace the text with your project id
 client = scratch3.CloudRequests(conn,ignore_exceptions=True) #The "ignore_exceptions" input does not let the server go down when a error occurs
 
 # When a request is send from the scratch project it will be detected with this line of code below
